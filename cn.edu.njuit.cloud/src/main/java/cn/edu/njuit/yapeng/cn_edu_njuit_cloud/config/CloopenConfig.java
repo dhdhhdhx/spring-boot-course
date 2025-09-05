7c698ea928bfb9c9db74f33b5b0beea413b5f2b3
ya peng zhou <1514921150@qq.com>
2025-09-05 22:23:10 +0800

    feat:新增cloud模块

---
+++ cn.edu.njuit.cloud/src/main/java/cn/edu/njuit/yapeng/cn_edu_njuit_cloud/config/CloopenConfig.java
@@ -0,0 +1,11 @@
+package cn.edu.njuit.yapeng.cn_edu_njuit_cloud.config;
+
+/**
+ * @Author: calm_sunset
+ * @Date: 2025/9/5
+ * @Version: 1.0
+ */
+
+
+public class CloopenConfig {
+}
---
+++ cn.edu.njuit.cloud/src/main/java/cn/edu/njuit/yapeng/cn_edu_njuit_cloud/service/SmsService.java
@@ -0,0 +1,2 @@
+package cn.edu.njuit.yapeng.cn_edu_njuit_cloud.service;public interface SmsService {
+}
